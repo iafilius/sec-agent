8a7d8ccb0764440d26dab0c8b0128619c01f83b3
Arjan Filius <iafilius@xs4all.nl>
2026-09-13 22:20:31 +0200

    chore(formula): update sec-agent formula to v2.13.0

--- Formula/sec-agent.rb
+++ Formula/sec-agent.rb
@@ -1,9 +1,9 @@
 class SecAgent < Formula
   desc "macOS Enclave-Bound Session Agent for Encrypted Secrets"
   homepage "https://github.com/iafilius/sec-agent"
-  url "https://github.com/iafilius/sec-agent/releases/download/v2.12.0/sec-agent_v2.12.0_darwin_arm64.tar.gz"
-  version "2.12.0"
-  sha256 "43e5817c20c56fed6d3b53fdbc669d18b258f6ab38abc4931a51a3eeb675a431"
+  url "https://github.com/iafilius/sec-agent/releases/download/v2.13.0/sec-agent_v2.13.0_darwin_arm64.tar.gz"
+  version "2.13.0"
+  sha256 "67f647be445b5485d160e088eb214c543413edf24e6e1db47e7020bc9989d8c4"
   license "GPL-3.0-or-later"
 
   depends_on :macos
